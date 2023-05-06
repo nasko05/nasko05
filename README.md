69c1409b626651c1a0a47916d33427d5d29d83aa
Atanas Donev <A.Donev@student.tudelft.nl>
2023-05-06 04:26:58 +0300

    Revert "Initial commit. Added skeleton structure for the project"
    This reverts commit daffa1449d23a61f27c8b13afa5631f7a9c398de.

--- README.md
+++
@@ -1,2 +0,0 @@
-To build `docker build -t my-express-app .`<br>
-To run `docker run -p 3000:3000 my-express-app`
--- src/models/user_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const {Schema} = require("mongoose");
-
-const User = mongoose.model('User', new Schema({
-    id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    }
-}))
